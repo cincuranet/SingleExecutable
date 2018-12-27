51653ee4a286f05b146216a482d622f4352773c5
Jiri Cincura <jiri@x2develop.com>
2018-12-27 16:15:08 +0100

    Support for compression

--- SingleExecutable/Definitions.cs
+++ SingleExecutable/Definitions.cs
@@ -8,6 +8,8 @@ namespace SingleExecutable
 		public const string PrefixDll = Prefix + "DLL" + Separator;
 		public const string PreExtractResourceName = Prefix + "PreExtract";
 		public const char PreExtractSeparator = ':';
+		public const string CompressionResourceName = Prefix + "Compression";
+		public const char CompressionSeparator = ':';
 		public const string LogFile = nameof(SingleExecutable) + ".log";
 		public const string LoggingEnvironmentVariable = "SE_DEBUG";
 	}
--- release.ps1
+++ release.ps1
@@ -2,7 +2,7 @@ Push-Location
 try {
 	& msbuild SingleExecutable\SingleExecutable.csproj /p:Configuration=Release /t:Rebuild /v:m
 	cd SingleExecutable\bin\Release
-	& .\SingleExecutable.exe -e SingleExecutable.exe -o SingleExecutable.out
+	& .\SingleExecutable.exe -e SingleExecutable.exe -o SingleExecutable.out -c
 	rm -Force * -Exclude SingleExecutable.out
 	mv SingleExecutable.out SingleExecutable.exe
 }
